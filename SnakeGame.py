256d0d19aceff4481ba94375403c3c0fdf2c16fa
HavenStay <nik-ignatkov@mail.ru>
2024-11-16 13:34:22 +0800

    q

--- SnakeGame.py
+++ SnakeGame.py
@@ -1,1 +1,15 @@
-##we
+import pygame
+import random
+
+pygame.display.set_mode((625,625))
+pygame.display.set_caption("SuperSnake")
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            quit()
+
+
+
+
+
