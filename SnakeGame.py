52b165ce6b9f6ad4bed290380a4c0985686f35e8
HavenStay <nik-ignatkov@mail.ru>
2024-11-19 20:57:09 +0800

    fix

--- SnakeGame.py
+++ SnakeGame.py
@@ -7,19 +7,21 @@ HEIGHT = 625
 WIDTH = 625
 GREEN = (0,255,0)
 RED = (255,0,0)
-
+WHITE = (255, 255, 255)
 
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("SuperSnake")
-
+screen.fill(WHITE)
 clock = pygame.time.Clock()
-FPS = 15
-Running = True
 
+FPS = 15
 x = HEIGHT // 2
 y = WIDTH // 2
-speed_snake = 15
+speed_snake = 25
+x_change = 0
+y_change = 0
 
+Running = True
 while Running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -28,14 +30,17 @@ while Running:
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 x -= speed_snake
+                x_change = 0
             elif event.key == pygame.K_RIGHT:
                 x += speed_snake
+                x_change = 0
             elif event.key == pygame.K_UP:
                 y -= speed_snake
+                y_change = 0
             elif event.key == pygame.K_DOWN:
                 y += speed_snake
+                y_change = 0
 
     pygame.draw.rect(screen, GREEN,[x,y,25,25])
     pygame.display.update()
     clock.tick(FPS)
-
